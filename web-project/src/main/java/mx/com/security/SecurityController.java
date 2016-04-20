fbccc1c60779bb6eee79cd3cdc2ef5460cc29708
gmtzs <gmtzs77@gmail.com>
2016-04-20 16:09:08 -0500

    cambios de prueba 2

--- web-project/src/main/java/mx/com/security/SecurityController.java
+++ web-project/src/main/java/mx/com/security/SecurityController.java
@@ -3,18 +3,17 @@ package mx.com.security;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import mx.com.security.dto.User;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import mx.com.security.dto.User;
 
 
 @Controller
--- web-project/src/test/java/AppTest.java
+++ web-project/src/test/java/AppTest.java
@@ -1,13 +1,41 @@
-import java.text.DateFormat;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
 
 
 public class AppTest {
 	
+	static Logger logger = Logger.getLogger(AppTest.class);
 	
 	public static void main(String[] args) {
+		String path = "\\Oracle\\Middleware\\user_projects\\domains\\cayte_dev_domain\\";
+		File file = new File(path + "holamundo.txt");
+		File file_test = new File(path + "holamundo2.txt");
+		try {
+			file.createNewFile();	
+			
+			boolean result = file.renameTo(file_test);
+			logger.info(file_test.getCanonicalPath());
+			logger.info(file.getCanonicalPath() + " resultado: " + result );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		String df = new java.text.SimpleDateFormat("HH:mm:ss").format(new java.util.Date());
-		System.out.println(df);
+		new AppTest().executeOperation();  
+	}
+	
+	public void displaySystemProperties(){
+		Properties prop = System.getProperties();
+		System.out.println(prop.getProperty("java.io.tmpdir"));
+		prop.list(System.out);
+	}
+	
+	public void executeOperation(){
+		Integer result = 4*10-6/2;
+		System.out.println(result);
 	}
 
 }
